3bd16b5ececa4209a2ebecc31eda6a4a733bb312
Devin Hight <devin.hight@shopify.com>
2023-02-18 00:23:13 -0700

    add earthTexture

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import { Suspense, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import {Canvas, useFrame, useLoader} from "@react-three/fiber";
 import { Stats, OrbitControls } from "@react-three/drei";
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import * as three from "three";
 import "./styles.css";
 
 const Cube = () => {
     const cube = useRef<three.Mesh>(null);
+    const textureEarth = useLoader(TextureLoader, '2k_earth_daymap.jpg')
+
 
     useFrame(() => {
-        cube.current!.rotation.x += 0.001;
+        // cube.current!.rotation.x += 0.007;
         cube.current!.rotation.y += 0.001;
     });
 
     return (
         <mesh ref={cube}>
             <sphereBufferGeometry args={[1, 15, 15]} />
-            <meshStandardMaterial color="#0391BA" />
+            <meshStandardMaterial color="#0391BA" map={textureEarth}/>
         </mesh>
     );
 };
 
 const Scene = () => {
+
     return (
         <>
             <gridHelper />
